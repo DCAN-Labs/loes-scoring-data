404a56a33295750b1912e80d428e16f2bb6d55e1
Paul Reiners <reine097@umn.edu>
2024-09-18 09:12:18 -0500

    Got rid of unused code.

--- src/anonymized_dataset.py
+++ src/anonymized_dataset.py
@@ -2,9 +2,6 @@
 
 import os
 import random
-import random
-from datetime import datetime, timedelta
-import pickle
 import shutil
 from pathlib import Path
 import shutil
@@ -47,32 +44,6 @@ def anonymize_dataset(bids_root_in, bids_root_out):
     participants_df.to_csv(os.path.join(bids_root_out, 'participants.tsv'), index=False, index_label=False)
 
     return participants_df
-
-
-def randomized_id(ids, copy_dir):
-    plain_text_to_anonymous = dict()
-    for subject in ids.keys():
-        random_subject = 'sub-'
-        for i in range(4):
-            rand_digit = chr(ord('0') + random.randint(0, 9))
-            random_subject = random_subject + rand_digit
-        for i in range(4):
-            rand_letter = chr(ord('A') + random.randint(0, 25))
-            random_subject = random_subject + rand_letter
-        plain_text_to_anonymous[subject] = random_subject
-        sessions = ids[subject]
-        first_time = datetime(1971, 1, 1, 0, 0, 0)
-        first_ses = 'ses-' + str(datetime(1971, 1, 1, 0, 0, 0))
-        encoded_sessions = [0]
-        for session in sessions[1:]:
-            later_time = datetime(int(session[4:8]), int(session[8:10]), int(session[10:12]))
-            difference = later_time - first_time
-            later_encoded_time = first_time + timedelta(days=difference.days)
-            later_encoded_time_ses = 'ses-' + str(later_encoded_time)
-            encoded_sessions.append(later_encoded_time_ses)
-
-    return plain_text_to_anonymous
-
 
 
 if __name__ == '__main__':
